bba7999191736ffcf1fbb1c322a22ebb171e011d
IreneGrisenti <irenegrisenti@proton.mem>
2026-09-04 10:11:48 +0200

    defined configuration for reporting

--- src/order_report/config.py
+++ src/order_report/config.py
@@ -1,6 +1,10 @@
-# Holds settings like:
-# - INPUT_FILE and OUTPUT_FOLDER
-# - required set of column names
-# 
-#
-#
+"""Definierar konfigurationen för rapportgenereringen."""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class ReportConfig:
+    """Sökvägar för att skapa en rapport."""
+    input_path: Path = Path("data/orders.csv")
+    output_path: Path = Path("output")
